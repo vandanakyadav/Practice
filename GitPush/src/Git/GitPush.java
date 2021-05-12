ea000ff16a861ec7191bd010d56063040b4b3650
prade <prade@DESKTOP-BU6HUEA>
2021-05-12 16:28:06 -0400

    push

---
+++ GitPush/src/Git/GitPush.java
@@ -0,0 +1,12 @@
+package Git;
+
+public class GitPush {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str = "Git Push";
+		System.out.println(str);
+	}
+
+}
